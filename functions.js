adf2181685c2c9ca0a5d225eab9b86e36ebad9a0
Victor <victorribas22@hotmail.com>
2023-02-22 11:29:28 -0300

    Added input

--- functions.js
+++ functions.js
@@ -1,11 +1,28 @@
-let telNumber = prompt("Digite o número de telefone");
-let message = prompt("Digite a mensagem");
+let telNumberInput = document.getElementById("tel");
+let messageInput = document.getElementById("message");
+let botao = document.getElementById("botao");
 let pLink = document.getElementById("hiperlink");
+let telNumber;
+let message;
 
-telNumberNoSpace = telNumber.replace(/ /g, "");
-messageReplaced = message.replace(/ /g, "%20");
+const handleChangeTel = (e) => {
+  telNumber = e.target.value;
+};
 
-let link = document.createElement("a");
-link.textContent = `api.whatsapp.com/send?phone=${telNumberNoSpace}&text=${messageReplaced}`;
-link.href = `api.whatsapp.com/send?phone=${telNumberNoSpace}&text=${messageReplaced}`;
-pLink.appendChild(link);
+const handleChangeMessage = (e) => {
+  message = e.target.value;
+};
+
+function clicou() {
+  telNumberNoSpace = telNumber.replace(/ /g, "");
+  messageReplaced = message.replace(/ /g, "%20");
+
+  let link = document.createElement("a");
+  link.textContent = `api.whatsapp.com/send?phone=${telNumberNoSpace}&text=${messageReplaced}`;
+  link.href = `api.whatsapp.com/send?phone=${telNumberNoSpace}&text=${messageReplaced}`;
+  pLink.appendChild(link);
+}
+
+telNumberInput.addEventListener("change", handleChangeTel);
+messageInput.addEventListener("change", handleChangeMessage);
+botao.addEventListener("click", clicou);
